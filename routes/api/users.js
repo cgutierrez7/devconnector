c74e5095f7c84e4fcce2dba42a247df759fb968b
Chris Gutierrez <chris.n.gutierrez@gmail.com>
2019-03-09 13:58:18 -0800

    models and api

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+//Load user model
+const User = require('../../models/User');
+
+
+// @route   POST api/user/register
+// @desc    Register user
+// @access  public
+
+
 router.get('/test', (req,res) => res.json({msg: 'Users api works'}));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
 const app = express();
 
 const db = require('./config/keys').mongoURI; //name as db because only for getting db 
 const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
 const profile = require('./routes/api/profile');
+
+// Body parser middleware
+app.use(bodyparser.urlencoded({ extended: false}));
+app.use(bodyparser.json());
+
 
 //connect to db (this would be 1 big line, but broken up to see what is going on)
 mongoose
